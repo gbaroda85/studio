bf2bb1a6ba99a6e9cf6326f3522e2d5c7d225d8d
Solanki Gaurav <gauravraj.3389@gmail.com>
2026-05-25 11:59:55 +0000

    YE KYA HAI KAISE HOGA

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-auth-domain",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-storage-bucket",
-  messagingSenderId: "placeholder-sender-id",
-  appId: "placeholder-app-id",
+  apiKey: "AIzaSyCxVyV8CgP_5_0YqU7_7S_5_0YqU7_7S",
+  authDomain: "gr7-multi-tools-kits.firebaseapp.com",
+  projectId: "gr7-multi-tools-kits",
+  storageBucket: "gr7-multi-tools-kits.firebasestorage.app",
+  messagingSenderId: "153258814798",
+  appId: "1:153258814798:web:1ec44e0541785501867c29",
+  measurementId: "G-N4P9M6S7X9"
 };
