a993b13e80e8525fe439e2217fd3057e752f0f28
Solanki Gaurav <gauravraj.3389@gmail.com>
2026-05-25 12:05:48 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCxVyV8CgP_5_0YqU7_7S_5_0YqU7_7S",
-  authDomain: "gr7-multi-tools-kits.firebaseapp.com",
-  projectId: "gr7-multi-tools-kits",
-  storageBucket: "gr7-multi-tools-kits.firebasestorage.app",
-  messagingSenderId: "153258814798",
-  appId: "1:153258814798:web:1ec44e0541785501867c29",
-  measurementId: "G-N4P9M6S7X9"
+  apiKey: "AIzaSyC-d57iJoWWi005_b-xuy1dya_il1RG7pE",
+  authDomain: "grs-multi-tools-kits.firebaseapp.com",
+  projectId: "grs-multi-tools-kits",
+  storageBucket: "grs-multi-tools-kits.firebasestorage.app",
+  messagingSenderId: "395729544318",
+  appId: "1:395729544318:web:290fe09b2beadbe0d5e42f"
 };
